e0a2cb9e0fe96433646e3ed58fb592299cce1642
K Sheenan <kaynesheenan+git@gmail.com>
2021-07-31 22:48:55 +0900

    defined invaders, used classList.add() to add .invader to all the 'alienInvaders in the array

--- app.js
+++ app.js
@@ -10,5 +10,14 @@ document.addEventListener('DOMContentLoaded', () => {
     let invaderId
 
 // define the alien invaders
+    const alienInvaders = [
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+        15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+        30, 31, 32, 33, 34, 35, 36, 37, 38, 39
+    ]
+
+    // draw the alien invaders
+    alienInvaders.forEach( invader => squares[currentInvaderIndex + invader].classList.add('invader'))
+
 
 })
