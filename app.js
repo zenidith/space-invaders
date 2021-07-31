45ab897a3d3697c116bac4bcb01fca5e6227fda1
K Sheenan <kaynesheenan+git@gmail.com>
2021-07-31 22:39:41 +0900

    begin JS

---
+++ app.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const squares = document.querySelectorAll('.grid div')
+    const resultDisplay = document.querySelector('#result')
+    let width = 15
+    let currentShooterIndex = 202
+    let currentInvaderIndex = 0
+    let alienInvadersTakenDown = []
+    let result = 0
+    let direction = 1
+    let invaderId
+
+// define the alien invaders
+
+})
